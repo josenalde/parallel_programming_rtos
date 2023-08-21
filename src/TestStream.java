c483ac77a46c94b54a99e7748feccc330fda37a0
josenaldeOliveira <josenalde@gmail.com>
2023-08-21 15:21:06 -0300

    Add files via upload

--- src/TestStream.java
+++ src/TestStream.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
 import java.util.List;
-//import java.util.concurrent.ForkJoinPool;
-
-//import org.junit.Test;
-
-//import static org.junit.Assert.assertEquals;
 
 public class TestStream {
     public static void main(String[] args) {
         // stream sequencial
-        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4);
-        listOfNumbers.stream().forEach(number ->
-        System.out.println(number + " " + Thread.currentThread().getName())
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4, 10, 20, 30, 40);
+        listOfNumbers
+            .stream()
+            .forEach(number ->
+                System.out.println(number + " " + Thread.currentThread().getName())
         );
-        System.out.println("\n");
+        System.out.println("Regiao Paralela: \n");
         // stream em paralelo
-        listOfNumbers.parallelStream().forEach(number ->
-        System.out.println(number + " " + Thread.currentThread().getName())
+        listOfNumbers
+            .parallelStream()
+            .forEach(number ->
+                System.out.println(number + " " + Thread.currentThread().getName())
         );
 
-        // soma em paralelo e reduz para sum, adicionando 5 à soma total
-        //int sum = listOfNumbers.parallelStream().reduce(0, Integer::sum) + 5;
-        //System.out.println(sum);
-        //assertEquals(sum).isNotEqualTo(15);         
+        // soma em paralelo e reduz para sum, adicionando 5 à soma total (0 valor inicial)
+        int sum = listOfNumbers
+            .parallelStream()
+            .reduce(0, Integer::sum) + 5; // lambda: (subtotal, element) -> subtotal + element)
+        System.out.println(sum);
 }
 }
