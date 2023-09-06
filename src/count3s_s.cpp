0775d8c4c29e1a3925b1bb8eccea1d6485b0919b
josenaldeOliveira <josenalde@gmail.com>
2023-09-06 14:23:05 -0300

    Add files via upload

--- src/count3s_s.cpp
+++ src/count3s_s.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 
-#define MAX 50
+#define MAX 1000000
 int v[MAX];
 int length = MAX;
 int count;
@@ -14,7 +14,7 @@ int main() {
     srand(time(NULL));
     for (int i=0; i<length; i++) {
         v[i] = rand() % 4;
-        cout << v[i] << ",";
+        //cout << v[i] << ",";
     }
     cout << endl << count3s() << " found";
     return 0;
@@ -23,8 +23,15 @@ int main() {
 int count3s() {
     int i;
     count=0;
+    clock_t tstart, tend;
+    tstart = clock(); //tempo de cpu (cpu time), não inclui delays nem espera de IO
     for(i=0;i<length;i++) {
         if(v[i]==3) count++;
     }
+    tend = clock();
+    double trun;
+    trun = (double)(tend - tstart)/(double)CLOCKS_PER_SEC;
+    cout.precision(12);
+    cout << fixed << "Time: " << trun << endl;
     return count;
 }
--- src/pthread_count3s_1.cpp
+++ src/pthread_count3s_1.cpp
@@ -2,19 +2,26 @@
 #include <pthread.h>
 #include <cstdlib>
 #include <ctime>
+//#include <boost/timer/timer.hpp>
 
 using namespace std;
 
-#define N 64
-#define T 4
+#define N 1000000
+/*Processador Intel® Core™ i7-10510U quad-core com 2 threads por core: 
+  Núcleo é um termo de hardware que descreve o número de unidades de processamento central independentes em um único componente de 
+  computação (matriz ou chip).
+  Um thread, ou thread de execução, é um termo de software para a sequência básica ordenada de instruções que pode ser passada
+  ou processada por um único núcleo de CPU.
+*/
+#define T 8 //máximo para a CPU em questão
 
 int v[N], count;
 
 void* count3s_thread(void* id) {
     int my_id = (int) id;
-    cout << "criei thread " << my_id;
+    //cout << "criei thread " << my_id;
     pthread_t thId = pthread_self();
-    printf(" with ID: %d\n", thId);
+    //printf(" with ID: %d\n", thId);
     int length_per_thread = N/T;
     int start = my_id*length_per_thread;
     for (int i=start; i<start+length_per_thread;i++) {
@@ -24,21 +31,27 @@ void* count3s_thread(void* id) {
 }
 
 int main()  {
+    //boost::timer::auto_cpu_timer t;
     pthread_t tid[T]; //array de threads
     int err;
     srand(time(NULL));
     for (int i=0; i<N; i++) {
         v[i] = rand() % 4;
-        cout << v[i] << ",";
+        //cout << v[i] << ",";
     }
-
+    clock_t tstart, tend;
+    tstart = clock();
     for (int i=0; i<T; i++) {
         err = pthread_create(&tid[i], NULL, count3s_thread, (void*) i);
     }
     for (int i=0; i<T; i++) {
         err  = pthread_join(tid[i], NULL);
     }
-    
+    tend = clock();
+    double trun;
+    trun = (double)(tend - tstart)/(double)CLOCKS_PER_SEC;
+    cout.precision(12);
+    cout << fixed << "Time: " << trun << endl;
     cout << endl << count << " found";
     return 0;
 }
