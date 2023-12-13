a45af8e39ef0f024ccfbd14ec8b3c1d916b9087f
josenaldeOliveira <josenalde@gmail.com>
2023-12-13 17:23:35 -0300

    Update fan_pid_control.ino

--- src/fan_pid_control/fan_pid_control.ino
+++ src/fan_pid_control/fan_pid_control.ino
@@ -3,7 +3,7 @@
 unsigned long lastTime;
 double u=0, y=0, intSetpoint, rpmSetpoint; // u: input, y: output (and sensor measure), sp: setpoint
 double iTerm=0, lastY=0;
-double kp, ki, kd, alpha=0.45;
+double kp, ki, kd;
 unsigned long sampleTime = 1000; // 1000 ms or 1 sec
 double uMin, uMax; // control signal saturation
 double voltageY;
@@ -12,13 +12,14 @@ double rpmInput;
 volatile double aux; //Váriavel que armazena as rotações. manipulada pela interrupção
 
 double VCC = 3.3; // For ESP32 3.3, For Uno 5
-int yPin = 26; //GPIO26  
-//A0 for sensor pin (pv sensor)
-int sensorPin = 25; // PWM or DAC0 (25) and DAC1 (26)
-int in1Pin = 12;
-int in2Pin = 14;
+int yPin = 26; //GPIO26  PWM output to fan blue wire
+int sensorPin = 25; // digital tachmeter (hall efect sensor)
+/* pins used only for driver L298N
+//int in1Pin = 12;
+//int in2Pin = 14;
+*/
 
-const int rpmMax = 2970;
+const int rpmMax = 1920; // apply 255 to PWM wire and measure the rpm max
 
 // ISR fan rotations
 void countRPM() {
@@ -35,7 +36,7 @@ void computeU() {
         
         // read speed signal and convert it to RPM (bipolar hall effect sensor)
         // rpmInput*60/2, since its 2 readings each cycle
-        y = map(rpmInput*30, 0, rpmMax, 0, 255); 
+        y = map(rpmInput*30, 0, rpmMax, 0, 255); // 8 bits
 
         double e = intSetpoint - y; // tracking error
         iTerm += (ki * e); // integral term I
@@ -44,10 +45,7 @@ void computeU() {
         else if (iTerm < uMin) iTerm = uMin;
         // y difference term
         double dY = (y - lastY);
-        //double filter_D = 1 + (alpha/sampleTime)*(-dY);
-        //D = kd*(-dY) / (filter_D)
-        // compute control signal
-        //u = kp*e + iTerm - (kd*dY/filter_D);
+        
         u = kp*e + iTerm - (kd*dY);
         if (u > uMax) u = uMax;
         else if (u < uMin) u = uMin;
@@ -89,49 +87,45 @@ void setup() {
   Serial.begin(115200);
   
   // ---------- configurations --------------------
-  //analogReadResolution(10); // equivalent to Uno
+  
   // configure fan speed sensor reading
   attachInterrupt(sensorPin, countRPM, RISING);//Habilita a interrupção 0 no pino 2. O UNO tem 02 interrupções
   
   // configure pwm output
   ledcAttachPin(yPin, 0); //channel 0 (pin 26)
-  ledcSetup(0, 25000, 8); //channel, freq (Hz), resolution (bits)
+  ledcSetup(0, 25000, 8); //channel, freq (25 kHz), resolution (bits)
   
   // set pin direction
   pinMode(sensorPin, INPUT);
-  pinMode(yPin, OUTPUT);
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
+  pinMode(yPin, OUTPUT); //PWM
+  // for L298N
+  //pinMode(in1Pin, OUTPUT);
+  //pinMode(in2Pin, OUTPUT);
   
   
-  rpmSetpoint = 2040; // considering 0-4095
+  rpmSetpoint = 1200; // max 1920 (avoid below 900)
   intSetpoint = map(rpmSetpoint, 0, rpmMax, 0, 255); // 8 bits
 
   setControlLimits(0, 255); // 255 = 3.3V PWM
-  setSampleTime(100);
-  setTunings(2, 0.8, 0.5);
-  //setTunings(1.41, 1.41/4.04, kp*2.92 );
+  setSampleTime(1000);
+  setTunings(1.41, 1.41/4.04, kp*2.92 );
 
-  // set forward movement
-  digitalWrite(in1Pin, LOW);
-  digitalWrite(in2Pin, HIGH);
+  // set forward movement L298N
+  //digitalWrite(in1Pin, LOW);
+  //digitalWrite(in2Pin, HIGH);
   
 }  
                             
 void loop() { //super loop
-  //y = analogRead(yPin); // 0 - 4095
-  // voltageInput
-  //voltageY = (VCC/4096)*y;
   computeU();
   ledcWrite(0, u); //ESP32 PWM
-  //dacWrite(uPin, u);
-  Serial.print(4096);
+  Serial.print(rpmMax);
   Serial.print(" ");
   Serial.print(0);
   Serial.print(" ");
-  Serial.print(y);
+  Serial.print(rpmInptut*30);
   Serial.print(" ");
-  Serial.print(intSetpoint);
-  Serial.println();  //Serial.println(kp);
+  Serial.print(rpmSetpoint);
+  Serial.println();  
   delay(10);
 }
