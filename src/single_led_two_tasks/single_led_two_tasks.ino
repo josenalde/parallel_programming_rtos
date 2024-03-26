09bf7c5cc8c308f17c0add3a8f280c37a5b22d42
josenaldeOliveira <josenalde@gmail.com>
2024-03-25 22:29:42 -0300

    Update single_led_two_tasks.ino

--- src/single_led_two_tasks/single_led_two_tasks.ino
+++ src/single_led_two_tasks/single_led_two_tasks.ino
@@ -1,5 +1,7 @@
-# include <freertos/FreeRTOS.h>
-# include <freertos/task.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "esp_log.h" //apenas para ESP-IDF
+#include "esp_err.h" //apenas para ESP-IDF
 
 /**
  Adaptado de Shawn Hymel, Dec. 3, 2020 - Piscar LED com intervalos diferentes em tarefas diferentes
@@ -9,44 +11,83 @@
 // LED invervalos de piscada
 static const int rate_1 = 500;  // ms
 static const int rate_2 = 300;  // ms
-
+static TaskHandle_t tLed1 = NULL; //handle para task
 // Pins
-static const int led_pin = 2;
+static const int led_pin = 22;
+//#define RUNS 5
+//volatile int i = 0;
+
+//#define TASK_1_STACK_SIZE       (configMINIMAL_STACK_SIZE * 4)
+//#define TASK_1_PRIORITY         (tskIDLE_PRIORITY  + 1)
 
 // t1: piscar led com rate_1
 void toggleLED_1(void *parameter) {
+  Serial.print("Primeira chamada de ");
+  Serial.println(__func__);
   while(1) {
     digitalWrite(led_pin, HIGH);
     vTaskDelay(rate_1 / portTICK_PERIOD_MS);
     digitalWrite(led_pin, LOW);
     vTaskDelay(rate_1 / portTICK_PERIOD_MS);
+    Serial.print(pcTaskGetTaskName(NULL));
+    Serial.print(" rodando no núcleo ");
+    Serial.print(xPortGetCoreID());
+    Serial.print(" com prioridade: ");
+    Serial.println(uxTaskPriorityGet(NULL));
+    //ESP_LOGW("main", "%s tick :  %d", __func__, xTaskGetTickCount()); //esp-idf
   }
 }
 
 // t2: piscar led com rate_2
 void toggleLED_2(void *parameter) {
+  Serial.print("Primeira chamada de ");
+  Serial.println(pcTaskGetTaskName(NULL));
   while(1) {
     digitalWrite(led_pin, HIGH);
     vTaskDelay(rate_2 / portTICK_PERIOD_MS);
     digitalWrite(led_pin, LOW);
     vTaskDelay(rate_2 / portTICK_PERIOD_MS);
+    Serial.print(pcTaskGetTaskName(NULL));
+    Serial.print(" rodando no núcleo ");
+    Serial.print(xPortGetCoreID());
+    Serial.print(" com prioridade: ");
+    Serial.println(uxTaskPriorityGet(NULL));
+    // Se quiser executar uma só vez ou a depender de uma condição/counter global etc.
+    //if (i == RUNS) vTaskDelete(NULL);
+    //i++; //global
+    //ESP_LOGI("main", "%s tick :  %d", __func__, xTaskGetTickCount()); //esp-idf
+
   }
 }
 
 void setup() {
 
+  Serial.begin(115200);
+  vTaskDelay(pdMS_TO_TICKS(1000));
   // Configura pin
   pinMode(led_pin, OUTPUT);
+  Serial.print(pcTaskGetTaskName(NULL)); //quem chamou foi o loop()
+  Serial.print(" ... Estou no SETUP rodando no nucleo ");
+  Serial.print(xPortGetCoreID());
+  Serial.print(" com prioridade: ");
+  Serial.println(uxTaskPriorityGet(NULL));
+  /*
+    TESTES
+    a) núcleos diferentes
+    b) mesmo núcleo com mesma prioridade
 
+    
+  */
+  
   // Tarefas em loop infinito
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
               toggleLED_1,  // função a ser chamada
               "Toggle 1",   // nome da task
               1024,         // tamanho em bytes
               NULL,         // parâmetros a passar a função
-              0,            // Prioridade (0 a configMAX_PRIORITIES - 1)
-              NULL,         // handle da task
-              1);     // núcleo: 0 (pro) 1 (app)
+              2,            // Prioridade (0 a configMAX_PRIORITIES - 1)
+              &tLed1,         // handle da task
+              0);     // núcleo: 0 (pro) 1 (app)
 
   // Tarefas em loop infinito
   xTaskCreatePinnedToCore(  
@@ -56,13 +97,19 @@ void setup() {
               NULL,         
               1,            
               NULL,         
-              1);     
+              0);     
 
-  // If this was vanilla FreeRTOS, you'd want to call vTaskStartScheduler() in
-  // main after setting up your tasks.
+  vTaskDelete(NULL); // Deleta loop(), ou seja, quem chamou setup() (TESTE COMENTAR)
+  // Adicionar um delay no setup() se task rodam no mesmo núcleo garante que as tarefas serão executadas
 }
 
 void loop() {
   // Diferente da abordagem super loop, com RTOS loop() não precisa fazer nada. Mas pode-se colocar algum vtaskdelay para que tarefas de menor prioridade acessem
   // setup() e loop() rodam suas tarefas com prioridade 1 no núcleo 1 no esp32
+  Serial.print("Loop rodando com prioridade: ");
+  Serial.print(uxTaskPriorityGet(NULL));
+  Serial.print(" no núcleo : ");
+  Serial.println(xPortGetCoreID());
+  //Serial.println(pcTaskGetTaskName(NULL)); //return loopTask
+
 }
