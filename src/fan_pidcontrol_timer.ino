cc937332cbd844a0bf826510b15df2fcde3e1321
josenaldeOliveira <josenalde@gmail.com>
2024-07-18 21:25:50 -0300

    Update fan_pidcontrol_timer.ino

--- src/fan_pidcontrol_timer.ino
+++ src/fan_pidcontrol_timer.ino
@@ -4,6 +4,7 @@
 
 // Settings
 static const TickType_t rpsInterval = 1000 / portTICK_PERIOD_MS;  // 1000ms = 1s (timer for getting pulses) - rotation per second
+static const TickType_t controlSampleTime = 10 / portTICK_PERIOD_MS;  
 
 unsigned long lastTime;
 double u = 0, y = 0, intSetpoint, rpmSetpoint; // u: input, y: output (and sensor measure), sp: setpoint
@@ -13,8 +14,8 @@ unsigned long sampleTime; //after getting a new speed calculate a new control si
 double uMin, uMax; // control signal saturation
 double voltageY;
 
-double rpmInput;
-volatile double pulse; //Variable for counting rotating pulses
+int rpmInput;
+volatile int pulse; //Variable for counting rotating pulses
 
 double VCC = 3.3; // For ESP32 3.3
 int yPin = 25; //GPIO25 PWM
@@ -23,7 +24,7 @@ int sensorPin = 27;
 int in1Pin = 12;
 int in2Pin = 14;
 
-const int rpmMax = 2970; //it requires validation - apply 255 to ENABLE A and measure speed RPM
+long rpmMax = 4080; //it requires validation - apply 255 to ENABLE A and measure speed RPM
 
 // ISR fan rotations
 void IRAM_ATTR countPulses() {
@@ -33,6 +34,11 @@ void IRAM_ATTR countPulses() {
 // Globals
 static TimerHandle_t getRPMTimer = NULL;
 static TimerHandle_t generatePIDControlTimer = NULL;
+
+void setTunings(double kP, double kI, double kD);
+void setSampleTime(int newSampleTime);
+void setControlLimits(double min, double max);
+float float_map(float x, float in_min, float in_max, float out_min, float out_max);
 
 //*****************************************************************************
 // Callbacks
@@ -44,7 +50,9 @@ void getRPMCallback(TimerHandle_t xTimer) {
 }
 
 void generatePIDControlCallback(TimerHandle_t xTimer) {
-    y = map(rpmInput, 0, rpmMax, 0, 255); 
+    // get PID parameters - potentiometer
+    setTunings(float_map(analogRead(33), 0, 4095, 0.1, 4.0), float_map(analogRead(32), 0, 4095, 0.1, 4.0), 0.0);
+    y = map(rpmInput, 0, rpmMax, 0, 255);
     double e = intSetpoint - y; // tracking error
     iTerm += (ki * e); // integral term I
     // integral term is always increasing, therefore it is limited
@@ -55,25 +63,24 @@ void generatePIDControlCallback(TimerHandle_t xTimer) {
     u = kp*e + iTerm - (kd*dY);
     if (u > uMax) u = uMax;
     else if (u < uMin) u = uMin;
-    ledcWrite(0, u); //ESP32 PWM - ENABLE A L298D
+    analogWrite(yPin, u); //ESP32 PWM - ENABLE A L298D
 }
 
 void taskPrintGraph(void *parameters) {
-  Serial.print(4096);
-  Serial.print(" ");
-  Serial.print(0);
-  Serial.print(" ");
-  Serial.print(y);
-  Serial.print(" ");
-  Serial.print(intSetpoint);
-  Serial.println(); 
-  vTaskDelay(pdMS_TO_TICKS(100));
+  while (true) {
+    Serial.print(255);
+    Serial.print(" ");
+    Serial.print(0);
+    Serial.print(" ");
+    Serial.print(y);
+    Serial.print(" ");
+    Serial.print(intSetpoint);
+    Serial.println(); 
+    //Serial.println(rpmInput);
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
 }
-
-void setTunings(double kP, double kI, double kD);
-void setSampleTime(int newSampleTime);
-void setControlLimits(double min, double max);
-              
+             
 void setup() {
   Serial.begin(115200);
   
@@ -86,7 +93,7 @@ void setup() {
 
   generatePIDControlTimer = xTimerCreate(
     "generatePIDControlTimer",    // Name of timer
-    samplingTime,           // Period of timer (in ticks)
+    controlSampleTime,           // Period of timer (in ticks)
     pdTRUE,                     // Auto-reload TRUE, one_shot FALSE
     (void *)1,                  // Timer ID
     generatePIDControlCallback);  // Callback function
@@ -96,15 +103,19 @@ void setup() {
   attachInterrupt(sensorPin, countPulses, RISING);
   
   // configure pwm output
-  ledcAttachPin(yPin, 0); //channel 0 (pin 25)
-  ledcSetup(0, 25000, 8); //channel, freq (Hz), resolution (bits)
-  
+  analogWriteResolution(yPin, 8);
+  analogWriteFrequency(yPin, 30000);
+
+  //ledcAttach(yPin, 25000, 8); //(pin 25) ledcAttachPin DEPRACTED - Arduino-ESP32 LEDC API
+    
   // set pin direction
   pinMode(sensorPin, INPUT);
   pinMode(yPin, OUTPUT);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
-  
+  pinMode(33, INPUT);
+  pinMode(32, INPUT);
+    
   // set forward movement
   digitalWrite(in1Pin, HIGH);
   digitalWrite(in2Pin, LOW);
@@ -115,21 +126,23 @@ void setup() {
   setControlLimits(0, 255); // 255 = 3.3V PWM
   
   //************************/
-  setSampleTime(1002);
+  setSampleTime(10);
   //********************* */
   
-  setTunings(2, 0.8, 0.5);
-  //setTunings(1.41, 1.41/4.04, kp*2.92 ); 
+  //setTunings(0.1, 1.2, 0.01);
+  //setTunings(2, 5, 0.8); 
 
   // Start Printing task
   xTaskCreatePinnedToCore(taskPrintGraph,
                           "taskPrintGraph",
                           1024,
                           NULL,
-                          1,
+                          2,
                           NULL,
                           0);
-  
+
+  xTimerStart(getRPMTimer, 0);
+  xTimerStart(generatePIDControlTimer, 0); 
 }  
                             
 void loop() { 
@@ -161,3 +174,9 @@ void setControlLimits(double min, double max) {
     if (iTerm > uMax) iTerm = uMax;
     else if (iTerm < uMin) iTerm = uMin;
 }
+
+float float_map(float x, float in_min, float in_max, float out_min, float out_max) {
+   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+ 
